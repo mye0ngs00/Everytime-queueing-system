4cb233b931da241d4be39472eaaacab9a9ad656b
son <ourendpoint@gmail.com>
2021-06-27 23:54:26 +0900

    feat: 로그인

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,5 +1,22 @@
-describe('테스트', () => {
-  it('테스트', () => {
-    expect(true).to.be.true
+describe('Macro', () => {
+  const signInfo = Cypress.env()
+  before(() => {
+    cy.visit(`https://everytime.kr/`)
+  })
+
+  it('Move to sign page', () => {
+    cy.get('.login')
+      .contains('로그인')
+      .click()
+  })
+
+  it('Sign in', () => {
+    cy.get('input[name=userid]')
+      .type(signInfo.id)
+    cy.get('input[name=password]')
+      .type(signInfo.password)
+    cy.get('input[type=submit]')
+      .contains('로그인')
+      .click()
   })
 })
