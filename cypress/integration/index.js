f8e2021e0d864e4c16e1a00fded7a53ed4ba0be9
son <ourendpoint@gmail.com>
2021-06-28 18:46:08 +0900

    feat: 글 입력폼 작성

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,13 +1,14 @@
 describe('Macro', () => {
-  const signInfo = Cypress.env()
+  const signInfo = Cypress.env('signInfo')
+  const boardUri = Cypress.env('board')
+  const queue = [
+    {
+      title: '제목으 ㄴ아무거나 입력할게요',
+      article: '안녕하세요 저는 정보통신학부 16학번 윤딴딴 입니다.'
+    }
+  ]
   before(() => {
-    cy.visit(`https://everytime.kr/`)
-  })
-
-  it('Move to sign page', () => {
-    cy.get('.login')
-      .contains('로그인')
-      .click()
+    cy.visit(`https://everytime.kr/${boardUri}`)
   })
 
   it('Sign in', () => {
@@ -18,5 +19,16 @@ describe('Macro', () => {
     cy.get('input[type=submit]')
       .contains('로그인')
       .click()
+  })
+
+  it('Write articles', () => {
+    queue.map((item) => {
+      cy.get('a#writeArticleButton')
+        .click()
+      cy.get('input[name=title]')
+        .type(item.title)
+      cy.get('textarea[name=text]')
+        .type(item.article)
+    })
   })
 })
