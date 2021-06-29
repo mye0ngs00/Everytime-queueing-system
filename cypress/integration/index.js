c52da4b02539e3d3df1b50296d48aed281a9fd98
son <ourendpoint@gmail.com>
2021-06-29 22:34:07 +0900

    feat: 게시글 작성 기능 추가

--- cypress/integration/index.js
+++ cypress/integration/index.js
@@ -1,34 +1,35 @@
+const queue = [
+  {
+    title: '제목으 ㄴ아무거나 입력할게요',
+    article: '안녕하세요 dpqmfl타임'
+  },
+]
+
 describe('Macro', () => {
   const signInfo = Cypress.env('signInfo')
   const boardUri = Cypress.env('board')
-  const queue = [
-    {
-      title: '제목으 ㄴ아무거나 입력할게요',
-      article: '안녕하세요 저는 정보통신학부 16학번 윤딴딴 입니다.'
-    }
-  ]
-  before(() => {
+  beforeEach(() => {
     cy.visit(`https://everytime.kr/${boardUri}`)
   })
 
-  it('Sign in', () => {
-    cy.get('input[name=userid]')
-      .type(signInfo.id)
-    cy.get('input[name=password]')
-      .type(signInfo.password)
-    cy.get('input[type=submit]')
-      .contains('로그인')
-      .click()
-  })
-
-  it('Write articles', () => {
-    queue.map((item) => {
+  queue.map((item, idx) => {
+    it(`article${idx}`, () => {
+      cy.get('input[name=userid]')
+        .type(signInfo.id)
+      cy.get('input[name=password]')
+        .type(signInfo.password)
+      cy.get('input[type=submit]')
+        .contains('로그인')
+        .click()
+      
       cy.get('a#writeArticleButton')
         .click()
       cy.get('input[name=title]')
         .type(item.title)
       cy.get('textarea[name=text]')
         .type(item.article)
+      cy.get('li.submit')
+        .click()
     })
   })
 })
